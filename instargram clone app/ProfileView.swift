8104cf104ab8d1909103ae7f0de84357c98f0c35
SiyoungJung <hohahahohaho09@gmail.com>
2025-06-02 08:41:32 +0900

    .

--- instargram clone app/ProfileView.swift
+++ instargram clone app/ProfileView.swift
@@ -9,23 +9,36 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        HStack {
-            Image(systemName: "person.crop.circle")
-                .resizable()
-                .frame(width: 100, height: 100)
-                .foregroundColor(.blue)
-            VStack{
-                Text("username")
-                    .font(.title2)
-                    .foregroundColor(.black)
-                HStack{
-                    Text("posts: 0 followers: 0 following: 0")
-                        .foregroundColor(.gray)
+        ScrollView(.vertical, showsIndicators: false){
+            HStack {
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.blue)
+                VStack{
+                    Text("username")
+                        .font(.title2)
+                        .foregroundColor(.black)
+                    HStack{
+                        Text("posts: 0 followers: 0 following: 0")
+                            .foregroundColor(.gray)
+                                }
+                            }
+                        }
+            ForEach(0..<88) { _ in
+                HStack(alignment:.center) {
+                    ForEach(0..<3) { _ in
+                        VStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.gray)
+                                .frame(width: 140, height: 140)
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
-        }
-    }
-}
 
 #Preview {
     ProfileView()
